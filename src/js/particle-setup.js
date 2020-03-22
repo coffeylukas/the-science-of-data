62f87479143924fe8d4f3c886466f42cf3d14931
Lukas Coffey <coffeylukas@gmail.com>
2020-03-21 23:23:49 -0400

    Added particles back in

--- src/js/particle-setup.js
+++ src/js/particle-setup.js
@@ -3,5 +3,5 @@ import particleConfig from './particlesjs-config.js'
 
 export default function() {
     console.log('In case you\'re wondering, I\'m using particle.js!')
-    // particlesJS('particle-js', particleConfig)
+    particlesJS('particle-js', particleConfig)
 }
