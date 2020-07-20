42e5303d8b6ea2e2fa5082fec4cf2cb2a1abaa1f
Lukas Coffey <coffeylukas@gmail.com>
2020-07-20 18:45:18 -0400

    Another attempt at fixing jekyll

--- Gemfile
+++ Gemfile
@@ -9,5 +9,4 @@ group :jekyll_plugins do
     gem 'jekyll-sitemap'
     gem 'jekyll-seo-tag'
     gem "jekyll-postcss"
-    gem "jekyll-purgecss"
 end
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
       require("postcss-import"),
       require("tailwindcss")("./tailwind.config.js"),
-      require("autoprefixer")
+      require("autoprefixer"),
+      purgecss({
+        content: ['./**/*.html']
+      })
     ]
   }
